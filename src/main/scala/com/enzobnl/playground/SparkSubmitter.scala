1eceac7cad0aee09ac31d997a689e0c87cd339e7
Bonnal Enzo <enzo.bonnal@orange.com>
2018-10-24 12:08:09 +0200

    Init

---
+++ src/main/scala/com/enzobnl/playground/SparkSubmitter.scala
@@ -0,0 +1,13 @@
+package com.enzobnl.playground
+import sys.process._
+object SparkSubmitter {
+  def main(args: Array[String]): Unit = {
+    val mainClass = "com.enzobnl.wordcountsparkscala.WordCount"
+    val jar = "./target/wordcountsparkscala-1.0-SNAPSHOT.jar"
+    val in = "pom.xml"
+    val out = "output"
+    val toCall = s"c:/applications/anaconda3/scripts/spark-submit.cmd --class $mainClass $jar $in $out"
+    println(toCall)
+    toCall !
+  }
+}
