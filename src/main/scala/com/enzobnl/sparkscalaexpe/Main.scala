ee6a92c234378d4bbe845c63393f11d122372bcd
EnzoBnl <enzobonnal@gmail.com>
2019-10-01 18:34:21 +0200

    GraphX vs GraphFrame pageRanks benches

--- src/main/scala/com/enzobnl/sparkscalaexpe/Main.scala
+++ src/main/scala/com/enzobnl/sparkscalaexpe/Main.scala
@@ -3,6 +3,6 @@ package com.enzobnl.sparkscalaexpe
 import com.enzobnl.sparkscalaexpe.playground._
 
 object Main extends App {
-  val runnables: Seq[Runnable] = Seq(DatasetVsDataFrame)
+  val runnables: Seq[Runnable] = Seq(GraphxVsGraphFrames)
   for(torun <- runnables) torun.run
 }
--- src/main/scala/com/enzobnl/sparkscalaexpe/playground/GraphxVsGraphFrames.scala
+++ src/main/scala/com/enzobnl/sparkscalaexpe/playground/GraphxVsGraphFrames.scala
@@ -2,7 +2,7 @@ package com.enzobnl.sparkscalaexpe.playground
 
 import java.util
 
-import com.enzobnl.sparkscalaexpe.util.QuickSparkSessionFactory
+import com.enzobnl.sparkscalaexpe.util.{QuickSparkSessionFactory, Utils}
 import org.apache.spark.ml.feature.SQLTransformer
 import org.apache.spark.graphx.GraphLoader
 
@@ -11,20 +11,21 @@ import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SQLContext, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StructType
-
+import org.graphframes.GraphFrame
 
 
 object GraphxVsGraphFrames extends Runnable {
-
+  final val RESET_PROB: Double = 0.15
+  final val N_ITER: Int = 10
   lazy val spark: SparkSession = QuickSparkSessionFactory.getOrCreate()
-
-  override def run(): Unit = {
+  spark.sparkContext.setLogLevel("ERROR")
+  def execGraphX(): Array[(String, Double)] = {
     val sc = spark.sparkContext
     // Load the edges as a graph
     val sparkPath = "/home/enzo/Prog/spark/"
     val graph = GraphLoader.edgeListFile(sc, sparkPath + "data/graphx/followers.txt")
     // Run PageRank
-    val ranks = graph.pageRank(0.0001).vertices
+    val ranks = graph.staticPageRank(N_ITER, RESET_PROB).vertices
     // Join the ranks with the usernames
     val users = sc.textFile(sparkPath + "data/graphx/users.txt").map { line =>
       val fields = line.split(",")
@@ -34,6 +35,43 @@ object GraphxVsGraphFrames extends Runnable {
       case (id, (username, rank)) => (username, rank)
     }
     // Print the result
-    println(ranksByUsername.collect().mkString("\n"))
+    ranksByUsername.collect()
   }
+
+  def execGraphFrame(): Array[(String, Double)] = {
+    val sc = spark.sparkContext
+    // Load the edges as a graph
+    val sparkPath = "/home/enzo/Prog/spark/"
+    val edges = spark.read
+      .format("csv")
+      .option("delimiter", " ")
+      .load(sparkPath + "data/graphx/followers.txt")
+      .toDF("src", "dst")
+    val vertices = spark.read
+      .format("csv")
+      .option("delimiter", ",")
+      .load(sparkPath + "data/graphx/users.txt")
+      .toDF("id", "pseudo", "name")
+
+    val graph: GraphFrame = GraphFrame.fromEdges(edges)
+    val pageRankResult: GraphFrame = graph.pageRank.resetProbability(RESET_PROB).maxIter(N_ITER).run()
+    import spark.implicits._
+    pageRankResult
+      .vertices
+      .join(vertices, Seq("id", "id"))
+      .select("pseudo", "pageRank")
+      .as[(String, Double)]
+      .collect()
+  }
+
+  override def run(): Unit = {
+
+    val gx: Seq[(String, Double)] = Utils.time {execGraphX().toSeq}
+    val gf: Seq[(String, Double)] = Utils.time {execGraphFrame().toSeq}
+    println(gx)
+    println(gf)
+  }
+
+
+
 }
